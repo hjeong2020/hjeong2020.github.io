ddb5f3c58f0354abf3d0214043fc6494646cccac
hjeong2020 <64095720+hjeong2020@users.noreply.github.com>
2020-12-14 16:59:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,34 +9,31 @@ const errorMessage = document.getElementById("errorMessage");
 const pathToData = "chatTranscript.lines";
 
 var updateCallback = function(data) {
-  //var chatTranscript = data;
-  //console.log(chatTranscript);
-var value = data.newValue;
-  console.log("value is" + JSON.stringify(value));
-var updatedLine = value[value.length - 1];
-  console.log(updatedLine);
-  var customerMessage = updatedLine.text;
-  console.log(customerMessage);
-  console.log(updatedLine.source.toLowerCase());
-  if (updatedLine.source.toLowerCase() === "visitor") {
-    var url =
-      "https://www.omdbapi.com?t=" + customerMessage + "&apikey=479949eb";
-    console.log(url);
-    fetch(url)
-	    .then(function (response) {
-	    return response.json();
-    }).then(function (res) {
-	    console.log(res);
-	    document.getElementById("Title").innerHTML = res.Title;
-	    document.getElementById("Year").innerHTML = res.Year;
-	    document.getElementById("Rated").innerHTML = res.Rated;
-	    document.getElementById("Actors").innerHTML = res.Actors;
-	    document.getElementById("Genre").innerHTML = res.Genre;
-	    document.getElementById("Plot").innerHTML = res.Plot;
-    }).catch(function (error) {
-	    console.log("Error: " + error);
-    });
-  }
+	var value = data.newValue;
+	//console.log("value is" + JSON.stringify(value));
+	var updatedLine = value[value.length - 1];
+	console.log(updatedLine);
+	var customerMessage = updatedLine.text;
+	console.log(customerMessage);
+	console.log(updatedLine.source.toLowerCase());
+	if (updatedLine.source.toLowerCase() === "visitor") {
+		var url ="https://www.omdbapi.com?t=" + customerMessage + "&apikey=479949eb";
+		console.log(url);
+		fetch(url)
+			.then(function (response) {
+			return response.json();
+		}).then(function (res) {
+			console.log(res);
+			document.getElementById("Title").innerHTML = res.Title;
+			document.getElementById("Year").innerHTML = res.Year;
+			document.getElementById("Rated").innerHTML = res.Rated;
+			document.getElementById("Actors").innerHTML = res.Actors;
+			document.getElementById("Genre").innerHTML = res.Genre;
+			document.getElementById("Plot").innerHTML = res.Plot;
+		}).catch(function (error) {
+			console.log("Error: " + error);
+		});
+	}
 };
 
 var notifyWhenDone = function(err) {
